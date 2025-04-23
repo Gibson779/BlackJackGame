3d9f3e7129146be347064c0941104ab6a3cf92ee
rgib <regangib@btinternet.com>
2025-04-23 20:19:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
 
-if (sum<= 20){
+
+if (sum <= 20){
     console.log("Do you want to draw a new card")
 } else if (sum === 21){
     console.log("Wohhoo!")
     hasBlackJack = true
+    
 }else {
 console.log("you are out")
+isAlive = false
 
 }
 
 console.log(hasBlackJack)
+console.log(isAlive)
+
+
