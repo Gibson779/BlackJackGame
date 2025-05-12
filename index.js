6ca5a5a1345fc2e7e3294d39e73f0db0a394dc37
rgib <regangib@btinternet.com>
2025-05-12 21:17:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-let messageEl  = document.getElementById("message-el")
+let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl= document.getElementById("cards-el")
 
@@ -15,7 +15,12 @@ function startGame(){
 }
 
 function renderGame(){
-    cardsEl.textContent= "Cards:" + cards[0] + " " + cards[1]
+    cardsEl.textContent= "Cards:"
+
+    for (let i = 0; i < cards.length; i ++)    {
+    cardsEl.textContent += cards[i] + " "
+
+}
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20){
         message = ("Do you want to draw a new card")
