14606faaa9dea1c1a593c4f1f50189747b129c9f
rgib <regangib@btinternet.com>
2025-05-13 19:52:33 +0100

    Update

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard] // ordered list - array
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl= document.getElementById("cards-el")
 
+console.log(cards)
+
+
 function getRandomCard(){
-    return 5
+   
+    let randomNumber = Math.floor (Math.random() *13) +1  // 0-12
+     if (randomNumber > 10){
+        return 10
+     } else if (randomNumber === 1) {
+        return 11
+     } else {
+        return randomNumber
+     }
+
 }
 
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
