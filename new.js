3002050dbbd29967193645b2fbdba30fe37de551
rgib <regangib@btinternet.com>
2025-05-14 21:10:08 +0100

    Create new test.js

---
+++ new.js
@@ -0,0 +1,7 @@
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+console.log()
+
+for (let i = 0; i < largeCountries.length; i++) {
+    console.log( "-" + largeCountries[i] )
+}
