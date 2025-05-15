7776ac5b87acd77799a88bd2974c734427206deb
rgib <regangib@btinternet.com>
2025-05-15 20:19:23 +0100

    update.js

--- new.js
+++ new.js
@@ -1,7 +1,1 @@
-let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
-
-console.log()
-
-for (let i = 0; i < largeCountries.length; i++) {
-    console.log( "-" + largeCountries[i] )
-}
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
