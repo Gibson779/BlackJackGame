c48f25a82929f2bd3f927f7004f077bf2d6c65b4
rgib <regangib@btinternet.com>
2025-05-19 13:56:43 +0100

    Update new.js

--- new.js
+++ new.js
@@ -1,1 +1,9 @@
-let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+let hands = ["rock", "paper", "scissor"]
+
+function getHand() {
+
+    let randomIndex= Math.floor( Math.random() * 3 )
+    return hands [randomIndex]
+
+}
+console.log( getHand() )
